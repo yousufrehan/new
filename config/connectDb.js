fcb0f5a33d4d3f093060b0831cb615190d6ffcc4
Yousuf Rehan <103877505+yousufrehan@users.noreply.github.com>
2024-05-21 08:10:22 +0530

    Update connectDb.js
    security

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    await mongoose.connect('mongodb+srv://yousuf_76:mongo7620@cluster0.cloitjd.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('put your own mongo url');
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
